a027e6ab90c922af3bf2fba81ef4b4d19ffb0bb9
Dion Tryban <github@diontryban.com>
2022-10-08 22:44:47 -0400

    Open browser in new tab if mod key is pressed

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,8 @@ export default class MyPlugin extends Plugin {
 		this.registerView(WEB_BROWSER_VIEW_ID, (leaf) => new WebBrowserView(leaf));
 
 		// TODO: Replace ribbon icon with better way to open web browser tab.
-		this.addRibbonIcon('search', 'Web Browser', (evt: MouseEvent) => {
-			let leaf = this.app.workspace.getLeaf();
+		this.addRibbonIcon('search', 'Web Browser', (event: PointerEvent) => {
+			let leaf = this.app.workspace.getLeaf(process.platform === "darwin" ? event.metaKey : event.ctrlKey);
 			leaf.setViewState({ type: WEB_BROWSER_VIEW_ID, active: true });
 		});
 	}
