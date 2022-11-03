5e646bc57369826ab050d746785f81c79a18a175
Quorafind <bonopengate@gmail.com>
2022-11-03 22:45:46 +0800

    feat: support highlight whole url when click the bar

--- src/header_bar.ts
+++ src/header_bar.ts
@@ -15,6 +15,7 @@ export class HeaderBar {
         this.searchBar.addClass("web-browser-search-bar");
         parent.appendChild(this.searchBar);
 
+		// TODO: Would this be ok to use Obsidian add domlistener instead?
         this.searchBar.addEventListener("keydown", (event: KeyboardEvent) => {
             if (!event) { var event = window.event as KeyboardEvent; }
             if (event.key === "Enter") {
@@ -23,6 +24,19 @@ export class HeaderBar {
                 }
             }
         }, false);
+
+		// Use focusin to bubble up to the parent
+		// Rather than just input element itself.
+		this.searchBar.addEventListener("focusin", (event: FocusEvent) => {
+			this.searchBar.select();
+		})
+
+		// When focusout, unselect the text to prevent it is still selected when focus back
+		// It will trigger some unexpected behavior,like you will not select all text and the cursor will set to current position;
+		// The expected behavior is that you will select all text when focus back;
+		this.searchBar.addEventListener("focusout", (event: FocusEvent) => {
+			window.getSelection()?.removeAllRanges();
+		})
     }
 
     addOnSearchBarEnterListener(listener: (url: string) => void) {
