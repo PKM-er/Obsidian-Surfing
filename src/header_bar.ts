8a026a9832de704fd5c5cad0580ae8415bf67ef6
Quorafind <bonopengate@gmail.com>
2022-11-20 23:07:26 +0800

    chore: update creating header

--- src/header_bar.ts
+++ src/header_bar.ts
@@ -1,31 +1,34 @@
 export class HeaderBar {
-    private searchBar: HTMLInputElement;
-    private onSearchBarEnterListener = new Array<(url: string) => void>;
+	private searchBar: HTMLInputElement;
+	private onSearchBarEnterListener = new Array<(url: string) => void>;
 
-    constructor(parent: Element) {
-        // CSS class removes the gradient at the right of the header bar.
-        parent.addClass("web-browser-header-bar");
-        // Remove default title from header bar.
+	constructor(parent: Element) {
+		// CSS class removes the gradient at the right of the header bar.
+		parent.addClass("web-browser-header-bar");
+		// Remove default title from header bar.
 		// Use Obsidian API to remove the title.
 		parent.empty();
 
-        // Create search bar in header bar.
+		// Create search bar in header bar.
 		// Use Obsidian CreateEL method.
 		this.searchBar = parent.createEl("input", {
 			type: "text",
 			placeholder: "Search with DuckDuckGo or enter address",
-			cls: "web-browser-search-bar"});
+			cls: "web-browser-search-bar"
+		});
 
 		// TODO: Would this be ok to use Obsidian add domlistener instead?
-        this.searchBar.addEventListener("keydown", (event: KeyboardEvent) => {
+		this.searchBar.addEventListener("keydown", (event: KeyboardEvent) => {
 			// eslint-disable-next-line @typescript-eslint/no-unused-vars
-            if (!event) { const event = window.event as KeyboardEvent; }
-            if (event.key === "Enter") {
-                for (const listener of this.onSearchBarEnterListener) {
-                    listener(this.searchBar.value);
-                }
-            }
-        }, false);
+			if (!event) {
+				const event = window.event as KeyboardEvent;
+			}
+			if (event.key === "Enter") {
+				for (const listener of this.onSearchBarEnterListener) {
+					listener(this.searchBar.value);
+				}
+			}
+		}, false);
 
 		// Use focusin to bubble up to the parent
 		// Rather than just input element itself.
@@ -39,15 +42,15 @@ export class HeaderBar {
 		this.searchBar.addEventListener("focusout", (event: FocusEvent) => {
 			window.getSelection()?.removeAllRanges();
 		})
-    }
+	}
 
-    addOnSearchBarEnterListener(listener: (url: string) => void) {
-        this.onSearchBarEnterListener.push(listener);
-    }
+	addOnSearchBarEnterListener(listener: (url: string) => void) {
+		this.onSearchBarEnterListener.push(listener);
+	}
 
-    setSearchBarUrl(url: string) {
-        this.searchBar.value = url;
-    }
+	setSearchBarUrl(url: string) {
+		this.searchBar.value = url;
+	}
 
 	focus() {
 		this.searchBar.focus();
