e4b0f39bb9cb3b67e17b85f7f8bcc5cb2f24a1aa
Quorafind <bonopengate@gmail.com>
2022-11-28 17:32:35 +0800

    fix: should not trigger javascript when click on bookemarklets btn

--- src/surfingPluginSetting.ts
+++ src/surfingPluginSetting.ts
@@ -576,7 +576,8 @@ export class SurfingSettingTab extends PluginSettingTab {
 			cls: 'cm-url',
 			href: 'javascript:(function(){var%20i%20%3Ddocument.location.href%3B%20document.location.href%3D%22obsidian%3A%2F%2Fweb-open%3Furl%3D%22%20%2B%20encodeURIComponent%28i%29%3B})();'
 		})
-		bookmarkLetsBtn.addEventListener("click", () => {
+		bookmarkLetsBtn.addEventListener("click", (e) => {
+			e.preventDefault();
 			clipboard.writeText(`javascript:(function(){var%20i%20%3Ddocument.location.href%3B%20document.location.href%3D%22obsidian%3A%2F%2Fweb-open%3Furl%3D%22%20%2B%20encodeURIComponent%28i%29%3B})();`)
 			new Notice(t("Copy BookmarkLets Success"))
 		})
