576d08675fde3aedfe55f4393990da151837ac1a
Quorafind <bonopengate@gmail.com>
2024-01-20 20:43:22 +0800

    feat: support open file via in page search bar

--- src/component/suggester/searchSuggester.ts
+++ src/component/suggester/searchSuggester.ts
@@ -1,57 +1,123 @@
-import {TextInputSuggest} from "./suggest";
-import type {App} from "obsidian";
-import {t} from "src/translations/helper";
-import {SEARCH_ENGINES, SearchEngine} from "../../surfingPluginSetting";
-import {SurfingView} from "../../surfingView";
+import { TextInputSuggest } from "./suggest";
+import { App, FuzzyMatch, ItemView, prepareFuzzySearch, TFile } from "obsidian";
+import { t } from "src/translations/helper";
+import { SEARCH_ENGINES, SearchEngine } from "../../surfingPluginSetting";
+import { SurfingView } from "../../surfingView";
 import SurfingPlugin from "../../surfingIndex";
-import {getComposedUrl} from "../../utils/url";
+import { getComposedUrl } from "../../utils/url";
 
 export class SearchEngineSuggester extends TextInputSuggest<string> {
-	private searchEngines: SearchEngine[];
-	private searchEnginesString: string[] = [];
-	private plugin: SurfingPlugin;
+    private searchEngines: SearchEngine[];
+    private searchEnginesString: string[] = [];
+    private plugin: SurfingPlugin;
 
-	constructor(public app: App, plugin: SurfingPlugin, public inputEl: HTMLInputElement | HTMLTextAreaElement) {
-		super(app, inputEl);
+    private mode: 'web' | 'file' = 'web';
 
-		this.plugin = plugin;
-	}
+    private files: TFile[] = [];
 
-	getSuggestions(inputStr: string): string[] {
-		this.searchEnginesString = [];
+    private view: ItemView;
 
-		const currentDefault = this.plugin.settings.defaultSearchEngine;
-		this.searchEngines = [...SEARCH_ENGINES, ...this.plugin.settings.customSearchEngine].sort(function (x, y) {
-			return x.name.toLowerCase() == currentDefault.toLowerCase() ? -1 : y.name.toLowerCase() == currentDefault.toLowerCase() ? 1 : 0;
-		});
-		console.log(currentDefault, this.searchEngines);
-		this.searchEngines.forEach((item) => {
-			this.searchEnginesString.push(item.name);
-		})
+    constructor(public app: App, plugin: SurfingPlugin, public inputEl: HTMLInputElement | HTMLTextAreaElement, view: ItemView) {
+        super(app, inputEl);
 
-		return this.searchEnginesString;
-	}
+        this.plugin = plugin;
+        this.files = this.app.vault.getFiles();
+        this.view = view;
+    }
 
-	renderSuggestion(item: string, el: HTMLElement): void {
-		el.createEl("div", {
-			text: t("Search with") + item,
-			cls: "wb-search-suggestion-text"
-		})
-		el.classList.add("wb-search-suggest-item");
-	}
+    fuzzySearchItemsOptimized(query: string, items: string[]): FuzzyMatch<string>[] {
+        const preparedSearch = prepareFuzzySearch(query);
 
-	selectSuggestion(item: string): void {
-		const currentInputValue: string = this.inputEl.value;
+        return items
+            .map((item) => {
+                const result = preparedSearch(item);
+                if (result) {
+                    return {
+                        item: item,
+                        match: result,
+                    };
+                }
+                return null;
+            })
+            .filter(Boolean) as FuzzyMatch<string>[];
+    }
 
-		if (/^\s{0,}$/.test(currentInputValue)) return;
+    getSuggestions(inputStr: string): string[] {
+        if (inputStr.trim().startsWith('/')) {
+            this.mode = 'file';
 
-		const currentSearchEngine = this.searchEngines.find((engine) => engine.name === item);
-		console.log(currentSearchEngine, item);
+            const names = this.files.map((file) => file.path);
 
+            const query = this.fuzzySearchItemsOptimized(inputStr.slice(1), names)
+                .sort((a, b) => {
+                    return b.match.score - a.match.score;
+                }).map((match) => match.item);
 
-		const url = (currentSearchEngine ? currentSearchEngine.url : SEARCH_ENGINES[0].url);
+            return query;
+        }
 
-		const finalUrl = getComposedUrl(url, currentInputValue);
-		SurfingView.spawnWebBrowserView(false, {url: finalUrl});
-	}
+        this.mode = 'web';
+
+        this.searchEnginesString = [];
+
+        const currentDefault = this.plugin.settings.defaultSearchEngine;
+        this.searchEngines = [...SEARCH_ENGINES, ...this.plugin.settings.customSearchEngine].sort(function (x, y) {
+            return x.name.toLowerCase() == currentDefault.toLowerCase() ? -1 : y.name.toLowerCase() == currentDefault.toLowerCase() ? 1 : 0;
+        });
+
+        this.searchEngines.forEach((item) => {
+            this.searchEnginesString.push(item.name);
+        });
+
+        return this.searchEnginesString;
+    }
+
+    renderSuggestion(item: string, el: HTMLElement): void {
+        switch (this.mode) {
+            case 'web':
+                el.createEl("div", {
+                    text: t("Search with") + item,
+                    cls: "wb-search-suggestion-text"
+                });
+                el.classList.add("wb-search-suggest-item");
+                break;
+            case 'file':
+                el.createEl("div", {
+                    text: 'Open ' + item,
+                    cls: "wb-search-suggestion-text"
+                });
+                el.classList.add("wb-search-suggest-item");
+                break;
+        }
+    }
+
+    async selectSuggestion(item: string) {
+        const currentInputValue: string = this.inputEl.value;
+
+        if (currentInputValue.trim() === '') return;
+
+        switch (this.mode) {
+            case 'web': {
+                const currentSearchEngine = this.searchEngines.find((engine) => engine.name === item);
+                const url = (currentSearchEngine ? currentSearchEngine.url : SEARCH_ENGINES[0].url);
+
+                const finalUrl = getComposedUrl(url, currentInputValue);
+                SurfingView.spawnWebBrowserView(false, {url: finalUrl});
+                break;
+            }
+            case 'file': {
+                const file = this.files.find((file) => file.path === item);
+                console.log(file, this.view.leaf);
+                if (file) {
+                    await this.view.leaf.setViewState({
+                        type: 'markdown',
+                        state: {
+                            file: file.path,
+                        },
+                    });
+                }
+                break;
+            }
+        }
+    }
 }
